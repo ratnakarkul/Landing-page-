6480bf3604ddadd1b1c0207566facd3ced1a9205
ratnakarkul <117739871+ratnakarkul@users.noreply.github.com>
2022-11-30 23:45:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var menulist = document.getElementsByClassName("menu-list")
+var deskview = document.getElementById("desk-view");
+deskview[0].style.display="none";
+
+
+var show=1;
+function togglemenu(){
+    if(show===1){
+        menulist[0].style.display="inline";
+        show=0;
+    }
+    else{
+        menulist[0].style.display="none";
+        show=1;
+    }
+}
+
